959c0c33ca35415f22f05bb4048fddbc8d13cea3
Jesse Overright <jesseoverright@gmail.com>
2014-05-06 09:02:46 -0500

    Added Vagrantfile and database setup

---
+++ bootstrap.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+
+# set mysql root password
+sudo debconf-set-selections <<< 'mysql-server-5.5 mysql-server/root_password password rootpass'
+sudo debconf-set-selections <<< 'mysql-server-5.5 mysql-server/root_password_again password rootpass'
+
+# install apache, mysql, php
+apt-get update
+apt-get install -y apache2 mysql-server-5.5 php5-mysql php5
+
+# symlink DocRoot to vagrant
+rm -rf /var/www
+ln -fs /vagrant /var/www
+
+# set up the database
+if [ ! -f /var/log/databasesetup ];
+then
+    echo "CREATE USER 'weather'@'localhost' IDENTIFIED BY 'password'" | mysql -uroot -prootpass
+    echo "CREATE DATABASE weather_comments" | mysql -uroot -prootpass
+    echo "GRANT ALL ON weather_comments.* TO 'weather'@'localhost'" | mysql -uroot -prootpass
+    echo "flush privileges" | mysql -uroot -prootpass
+
+    touch /var/log/databasesetup
+
+    if [ -f /vagrant/src/comments_db.sql ];
+    then
+        mysql -uroot -prootpass weather_comments < /vagrant/src/comments_db.sql
+    fi
+fi
+
+# restart apache
+sudo service apache2 restart
---
+++ src/comments_db.sql
@@ -0,0 +1,40 @@
+-- phpMyAdmin SQL Dump
+-- version 4.0.4
+-- http://www.phpmyadmin.net
+--
+-- Host: localhost
+-- Generation Time: May 05, 2014 at 03:26 PM
+-- Server version: 5.6.12
+-- PHP Version: 5.5.1
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `t3`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `comments`
+--
+
+DROP TABLE IF EXISTS `comments`;
+CREATE TABLE IF NOT EXISTS `comments` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `location` varchar(255) NOT NULL,
+  `comment` text NOT NULL,
+  `date` datetime NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
