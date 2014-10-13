f16f061f3dfdd0262b9b25035e4ebfd448340f2a
Jesse Overright <jesseoverright@gmail.com>
2014-10-12 20:26:09 -0500

    Added comments controller

---
+++ app/controllers/CommentController.php
@@ -0,0 +1,55 @@
+<?php
+
+class CommentController extends \BaseController {
+
+	/**
+	 * Display a listing of the resource.
+	 *
+	 * @return Response
+	 */
+	public function index()
+	{
+		return Response::json( Comment::get() );
+	}
+
+
+	/**
+	 * Store a newly created resource in storage.
+	 *
+	 * @return Response
+	 */
+	public function store()
+	{
+		Comment::create( array(
+			'location'   => Input::get( 'location' ),
+			'comment'    => Input::get( 'comment' ),
+			'date'       => Input::get( 'date' ),
+			'conditions' => Input::get( 'conditions' )
+			)
+		);
+
+		return Response::json( array(
+			'success' => true
+			)
+		);
+	}
+
+
+	/**
+	 * Remove the specified resource from storage.
+	 *
+	 * @param  int  $id
+	 * @return Response
+	 */
+	public function destroy($id)
+	{
+		Comment::destroy( $id );
+
+		return Response::json( array (
+			'success' => true
+			)
+		);
+	}
+
+
+}
